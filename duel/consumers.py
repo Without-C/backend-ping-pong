eaf16a447e9dd3d4de471eb1d79db82d759b1083
Youngho Cho <dominoyh5@gmail.com>
2025-02-07 19:32:06 +0900

    fix: remove empty group

--- duel/consumers.py
+++ duel/consumers.py
@@ -31,28 +31,40 @@ matchmaking = Matchmaking(2)
 
 class DuelConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
-        # 소켓 연결
+        # 초기화
+        self.group_name = None
+
+        # 소켓 연결 허용
         await self.accept()
 
         # 대기 큐에 추가
         await matchmaking.add_waiting_participant(self.channel_name)
 
-        # 충분한 수의 대기자가 모인 경우 group 생성
+        # 충분한 수의 대기자가 모인 경우 매치매이킹
         match_result = await matchmaking.try_matchmaking()
         if match_result:
             group_name = f"match_{uuid.uuid4().hex}"
             for player_channel_name in match_result:
                 await self.channel_layer.group_add(group_name, player_channel_name)
 
-            await self.channel_layer.group_send(group_name, {"type": "group.message", "message": "match maked"})
+            # 매치매이킹이 이루어진 후 대상자들에게 그룹 이름 통보
+            await self.channel_layer.group_send(group_name, {"type": "group.assign", "group_name": group_name})
 
     async def disconnect(self, close_code):
+        # 대기자 큐에 있었다면 삭제
         await matchmaking.remove_waiting_participant(self.channel_name)
+
+        # 연결이 끊겼을 때 속해있던 그룹이 있으면 탈퇴
+        if self.group_name:
+            await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive_json(self, content):
         message = content["message"]
         await self.send_json({"message": message})
-
-    async def group_message(self, event):
-        message = event["message"]
-        await self.send_json({"message": message})
+    
+    async def group_assign(self, event):
+        """
+        매치매이킹이 이루어졌을 때 호출됨
+        """
+        self.group_name = event["group_name"]
+        await self.send_json({"message": "match maked"})
