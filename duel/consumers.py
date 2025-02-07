df88d70a52a4c602fbcb507f241524d7bec5f59a
Youngho Cho <dominoyh5@gmail.com>
2025-02-07 17:30:24 +0900

    feat: add Mathmaking class

--- duel/consumers.py
+++ duel/consumers.py
@@ -1,13 +1,35 @@
-import json
+import asyncio
 
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
+class Matchmaking:
+    def __init__(self, required_player_count):
+        self.lock = asyncio.Lock()
+        self.queue = []
+        self.required_player_count = required_player_count
+
+    async def add_waiting_participant(self, channel_name):
+        async with self.lock:
+            self.queue.append(channel_name)
+
+            print(self.queue)
+    
+    async def remove_waiting_participant(self, channel_name):
+        async with self.lock:
+            self.queue.remove(channel_name)
+
+            print(self.queue)
+
+matchmaking = Matchmaking(2)
+
 class DuelConsumer(AsyncJsonWebsocketConsumer):
     async def connect(self):
+        await matchmaking.add_waiting_participant(self.channel_name)
+
         await self.accept()
 
     async def disconnect(self, close_code):
-        pass
+        await matchmaking.remove_waiting_participant(self.channel_name)
 
     async def receive_json(self, content):
         message = content["message"]
